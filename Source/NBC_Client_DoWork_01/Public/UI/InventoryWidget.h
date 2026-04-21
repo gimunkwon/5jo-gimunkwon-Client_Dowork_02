26bb1c9fa387ad2cd82e92bb48edc7d5266a417e
gimunkwon <gimon71@gmail.com>
2026-04-21 11:46:42 +0900

    [add] PlayerWidget 클래스 추가

---
+++ Source/NBC_Client_DoWork_01/Public/UI/InventoryWidget.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InventoryWidget.generated.h"
+
+UCLASS()
+class NBC_CLIENT_DOWORK_01_API UInventoryWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};

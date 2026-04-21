c77c8248892caf12c7ebe36345bde8f64ca2e993
gimunkwon <gimon71@gmail.com>
2026-04-21 14:38:15 +0900

    [add] InventoryWidget 클래스 추가및 Grid방식으로 초기화

--- Source/NBC_Client_DoWork_01/Private/UI/InventoryWidget.cpp
+++ Source/NBC_Client_DoWork_01/Private/UI/InventoryWidget.cpp
@@ -1,1 +1,47 @@
 #include "UI/InventoryWidget.h"
+
+#include "Components/GridPanel.h"
+
+void UInventoryWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	Grid_MaxRow = 5;
+	Grid_MaxColumn = 6;
+	
+	if (Grid_Inven)
+	{
+		for (int32 Col = 0; Col < Grid_MaxColumn; ++Col)
+		{
+			Grid_Inven->ColumnFill.Add(1.f);
+		}
+		for (int32 Row = 0; Row < Grid_MaxRow; ++Row)
+		{
+			Grid_Inven->RowFill.Add(1.f);
+		}
+	}
+	
+	for (int32 Row = 0; Row < Grid_MaxRow; ++Row)
+	{
+		for (int32 Col = 0; Col < Grid_MaxColumn; ++Col)
+		{
+			if (WBP_InvenSlot)
+			{
+				UUserWidget* NewSlot = CreateWidget<UUserWidget>(this, WBP_InvenSlot);
+				if (NewSlot)
+				{
+					Grid_Inven->AddChildToGrid(NewSlot, Row, Col);
+				}
+				
+			}
+		}
+	}
+	
+	
+}
+
+void UInventoryWidget::UpdateInventoryItem(UMyPlayerInventory* InventoryInst)
+{
+	
+}
+
--- Source/NBC_Client_DoWork_01/Public/UI/InventoryWidget.h
+++ Source/NBC_Client_DoWork_01/Public/UI/InventoryWidget.h
@@ -4,8 +4,25 @@
 #include "Blueprint/UserWidget.h"
 #include "InventoryWidget.generated.h"
 
+class UGridPanel;
+class UMyPlayerInventory;
+
 UCLASS()
 class NBC_CLIENT_DOWORK_01_API UInventoryWidget : public UUserWidget
 {
 	GENERATED_BODY()
+	
+public:
+	virtual void NativeConstruct() override;
+	void UpdateInventoryItem(UMyPlayerInventory* InventoryInst);
+protected:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UGridPanel> Grid_Inven;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Widget")
+	TSubclassOf<UUserWidget> WBP_InvenSlot;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Grid")
+	int32 Grid_MaxRow;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Grid")
+	int32 Grid_MaxColumn;
 };

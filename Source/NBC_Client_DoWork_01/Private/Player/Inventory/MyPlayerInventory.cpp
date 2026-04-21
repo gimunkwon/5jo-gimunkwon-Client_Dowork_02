e2f2225f02d45b409ec02feabd50710936513508
gimunkwon <gimon71@gmail.com>
2026-04-21 17:22:36 +0900

    [fix] 위젯과 라이프사이클 문제때문에 AddInventoryItem 함수에서 Tarray에 FItemData를 넣은후 컨트롤러를 통해 인벤토리 위젯의 Update메서드를 사용하도록 수정

--- Source/NBC_Client_DoWork_01/Private/Player/Inventory/MyPlayerInventory.cpp
+++ Source/NBC_Client_DoWork_01/Private/Player/Inventory/MyPlayerInventory.cpp
@@ -1,14 +1,16 @@
 #include "NBC_Client_DoWork_01/Public/Player/Inventory/MyPlayerInventory.h"
 
+#include "Player/Controller/MyPlayerControlloer.h"
 
 
 UMyPlayerInventory::UMyPlayerInventory()
 {
 	PrimaryComponentTick.bCanEverTick = false;
 	
-	InventoryCapacity = 6;
+	InventoryCapacity = 30;
 	
-	Inventory.SetNum(InventoryCapacity);
+	Inventory.Reserve(InventoryCapacity);
+	
 }
 
 
@@ -17,9 +19,25 @@ void UMyPlayerInventory::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	Inventory.Emplace(ItemList.Potion);
-	Inventory.Emplace(ItemList.Gold);
-	Inventory.Emplace(ItemList.Sword);
+	AddInventoryItem();
+}
+
+void UMyPlayerInventory::AddInventoryItem()
+{
+	if (!ItemDataTable) return;
+	
+	static const FString ContextString(TEXT("ItemData"));
+	FItemDataTable* Row = ItemDataTable->FindRow<FItemDataTable>(TEXT("Gold"),ContextString);
+	FItemDataTable* Row_02 = ItemDataTable->FindRow<FItemDataTable>(TEXT("Sword"),ContextString);
+	
+	if (Row)
+	{
+		UE_LOG(LogTemp,Warning,TEXT("Add Success"));
+		if (AMyPlayerControlloer* PC  = Cast<AMyPlayerControlloer>(GetOwner()->GetInstigatorController()))
+		{
+			PC->GetInventoryWidget()->UpdateInventoryItem(this);
+		}
+	}
 }
 
 

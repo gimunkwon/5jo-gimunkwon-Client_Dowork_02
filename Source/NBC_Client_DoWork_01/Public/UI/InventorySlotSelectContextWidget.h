76ad6020f9ad4ef00544e4e7ea52f4eb1710a422
gimunkwon <gimon71@gmail.com>
2026-04-22 19:37:11 +0900

    [add] 인벤토리 슬롯에서 오른쪽 마우스 클릭시 컨텍스트 메뉴로 액션을 정할수있는 위젯클래스 추가및 로직 추가

--- Source/NBC_Client_DoWork_01/Public/UI/InventorySlotSelectContextWidget.h
+++ Source/NBC_Client_DoWork_01/Public/UI/InventorySlotSelectContextWidget.h
@@ -4,8 +4,33 @@
 #include "Blueprint/UserWidget.h"
 #include "InventorySlotSelectContextWidget.generated.h"
 
+class UMenuAnchor;
+class UButton;
+
 UCLASS()
 class NBC_CLIENT_DOWORK_01_API UInventorySlotSelectContextWidget : public UUserWidget
 {
 	GENERATED_BODY()
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnCloseButton);
+	
+public:
+	virtual void NativeConstruct() override;
+	
+	UFUNCTION()
+	void OnUseButtonClicked();
+	UFUNCTION()
+	void OnCloseButtonClicked();
+	UFUNCTION()
+	void OnDeleteButtonClicked();
+	
+	UPROPERTY()
+	FOnCloseButton OnCloseButton;
+	
+protected:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> Btn_UseItem;
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> Btn_DeleteItem;
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> Btn_Close;
 };

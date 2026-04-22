d269a00d01f7e78a95849b12d8a78dc2aded90ce
gimunkwon <gimon71@gmail.com>
2026-04-22 16:37:50 +0900

    [add] 인벤토리 슬롯에서 오른쪽 마우스 클릭시 활성화될 ContextMenuWidget클래스 추가

---
+++ Source/NBC_Client_DoWork_01/Public/UI/InventorySlotSelectContextWidget.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InventorySlotSelectContextWidget.generated.h"
+
+UCLASS()
+class NBC_CLIENT_DOWORK_01_API UInventorySlotSelectContextWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};

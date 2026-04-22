64ea473153a108427820c749a84c5feefa946d74
gimunkwon <gimon71@gmail.com>
2026-04-22 12:56:11 +0900

    [add] PlayerClass 에서 칭호를 관리할 Tset자료형 추가및 Beginplay에서 현재 칭호를 업데이트 하는 메서드 추가

--- Source/NBC_Client_DoWork_01/Private/Player/MyPlayer.cpp
+++ Source/NBC_Client_DoWork_01/Private/Player/MyPlayer.cpp
@@ -1,21 +1,43 @@
 #include "NBC_Client_DoWork_01/Public/Player/MyPlayer.h"
 
 #include "NBC_Client_DoWork_01/Public/Player/Inventory/MyPlayerInventory.h"
+#include "Player/Controller/MyPlayerControlloer.h"
 
 
 AMyPlayer::AMyPlayer()
 	: InventoryAC(nullptr)
 {
 	PrimaryActorTick.bCanEverTick = false;
-	
 	InventoryAC = CreateDefaultSubobject<UMyPlayerInventory>(TEXT("InventoryComponent"));
 	
+	PlayerTitle.Add(EPlayer_Title::Newbie);
+	PlayerTitle.Add(EPlayer_Title::Magician);
 }
 
 void AMyPlayer::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	const UEnum* Enumptr = StaticEnum<EPlayer_Title>();
+	
+	if (Enumptr)
+	{
+		for (TSet<EPlayer_Title>::TConstIterator It = PlayerTitle.CreateConstIterator(); It; ++It)
+		{
+			FString TitleName = Enumptr->GetNameStringByValue((int64)*It);
+			
+			UE_LOG(LogTemp,Warning,TEXT("Currnet Player Title : %s"), *TitleName);
+		}
+	}
+	
+	if (AMyPlayerControlloer* PC = Cast<AMyPlayerControlloer>(GetController()))
+	{
+		if (PC)
+		{
+			UE_LOG(LogTemp,Warning,TEXT("UpdatePlayerTitleText..."));
+			PC->GetInventoryWidget()->UpdatePlayerTitleText();
+		}
+	}
 }
 
 void AMyPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)

4ba892d76be0a1faf599249605a3b249870faf8f
gimunkwon <gimon71@gmail.com>
2026-04-22 21:10:31 +0900

    [add] 위젯에서 호출할 진짜 인벤토리 배열을 인덱스에서 찾아서 비워주는 메서드 추가

--- Source/NBC_Client_DoWork_01/Private/UI/InventorySlotWidget.cpp
+++ Source/NBC_Client_DoWork_01/Private/UI/InventorySlotWidget.cpp
@@ -3,6 +3,8 @@
 #include "PaperSprite.h"
 #include "Components/Button.h"
 #include "Components/Image.h"
+#include "Player/MyPlayer.h"
+#include "Player/Inventory/MyPlayerInventory.h"
 
 void UInventorySlotWidget::NativeConstruct()
 {
@@ -43,10 +45,27 @@ FReply UInventorySlotWidget::NativeOnMouseButtonDown(const FGeometry& InGeometry
 		UE_LOG(LogTemp,Warning,TEXT("오른쪽 마우스 클릭 이벤트 발생!!"));
 		if (OnRightClicked.IsBound())
 		{
-			OnRightClicked.Broadcast(ItemName);
+			OnRightClicked.Broadcast(ItemName,this);
 		}
 		return FReply::Handled();
 	}
 	
 	return FReply::Unhandled();
+}
+
+void UInventorySlotWidget::ClearSlotData(int32 index)
+{
+	ItemName = NAME_None;
+	
+	if (Image_Item)
+	{
+		Image_Item->SetBrushResourceObject(nullptr);
+	}
+	// 인벤토리 TArray 해당 배열 인덱스 값 초기화
+	if (AMyPlayer* MyOwner = Cast<AMyPlayer>(GetOwningPlayerPawn()))
+	{
+		MyOwner->GetInventory()->DeleteItemFromInventory(index);
+	}
+	
+	UE_LOG(LogTemp,Warning,TEXT("슬롯 데이터 초기화 완료"));
 }
